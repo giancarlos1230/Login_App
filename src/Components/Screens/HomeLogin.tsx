2a1b6a0ebb15b0ca9d6d3c1de23fafbe1df5495e
giancarlos1230 <gianjiron98@gmail.com>
2021-12-03 08:50:37 -0600

    New folder src, components and Screens, also a new file HomeLogin

---
+++ src/Components/Screens/HomeLogin.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import { StyleSheet, Text, View } from 'react-native'
+
+const HomeLogin = () => {
+    return (
+        <View>
+            <Text> Welcome </Text>
+        </View>
+    )
+}
+
+export default HomeLogin
+
+const styles = StyleSheet.create({})
